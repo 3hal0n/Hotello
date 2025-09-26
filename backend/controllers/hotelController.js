eaa1566ba2f69720e182880d6278280824c34b3f
Shalon Fernando <shalonfernando52@gmail.com>
2025-09-26 22:38:49 +0530

    Created postHotel

--- backend/controllers/hotelController.js
+++ backend/controllers/hotelController.js
@@ -45,3 +45,61 @@ export const getHotelById=async(req,res)=>{
         })
     }
 };
+
+//create new hotel
+export const postHotel=async(req,res)=>{
+    try{
+        const {userId}=getAuth(req);
+        if(!userId){
+            return res.status(401).json({
+                success:false,
+                message:"Unauthorized",
+            });
+        }
+        //get data from requst body
+        const {name,description,location,pricePerNight,amenities,images}=req.body;
+
+        //create stripe product
+        console.log("Creating Stripe Product");
+        const stripeProduct=await stripe.products.create({
+            name: name,
+            description:description || 'hotel accommodation',
+            images:images.map(img=>img.url),
+            default_price_data:{
+                currency:'lkr',
+                unit_amount:pricePerNight*100,
+                recurring:{interval:'day'},
+            },
+        });
+        console.log("Stripe Product Created:", stripeProduct.id);
+
+        //create hotel in db
+        const newHotel=new Hotels({
+            name:name,
+            ownerId:userId,
+            description:description,
+            location:location,
+            pricePerNight:pricePerNight,
+            amenities:amenities,
+            images:images,
+            stripePriceId:stripeProduct.default_price,
+            createdAt:Date.now(),
+        });
+
+        //save to db
+        const savedHotel=await newHotel.save();
+        console.log("Hotel Saved to DB:", savedHotel._id);
+        res.status(201).json({
+            success:true,
+            data:savedHotel,
+        });
+
+    }
+    catch(error){
+        console.error("Error in postHotel:", error);
+        res.status(500).json({
+            success:false,
+            message:"Server Error"
+        });
+    }
+}
